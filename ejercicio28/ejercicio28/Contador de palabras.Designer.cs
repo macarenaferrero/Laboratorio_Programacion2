0c57901e62a868bf565a3c40cd15a1d6d333006a
Macarena Ferrero <macarenaferrero@hotmail.com>
2020-09-17 00:28:38 -0300

    Ejercicio28

---
+++ ejercicio28/ejercicio28/Contador de palabras.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ejercicio28
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTxtPalabras = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTxtPalabras
+            // 
+            this.richTxtPalabras.Location = new System.Drawing.Point(-1, -3);
+            this.richTxtPalabras.Name = "richTxtPalabras";
+            this.richTxtPalabras.Size = new System.Drawing.Size(342, 263);
+            this.richTxtPalabras.TabIndex = 0;
+            this.richTxtPalabras.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(254, 266);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.Calcular_OnClick);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 296);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.richTxtPalabras);
+            this.Name = "FormPrincipal";
+            this.Text = "Contador de Palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTxtPalabras;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ ejercicio28/ejercicio28/Contador de palabras.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace ejercicio28
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        
+
+
+
+        private void Calcular_OnClick(object sender, EventArgs e)
+        {
+
+            Dictionary<string, int> Palabras = new Dictionary<string, int>();
+
+            string auxPalabras;
+            auxPalabras = this.richTxtPalabras.Text;
+            string[] variasPalabras;
+            variasPalabras = auxPalabras.Split(' ');
+            int contador = 1;
+
+            foreach (string palabra in variasPalabras)
+            {
+                if(!Palabras.ContainsKey(palabra))
+                {
+                    Palabras.Add(palabra, contador);
+                }
+                else
+                {
+                    Palabras[palabra]++;
+                }
+                contador = 1;
+                
+
+                MessageBox("Wd");
+            }
+        }
+    }
+}
