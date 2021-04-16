bd2dbeaff5a4fe5a3451b4be0707fb7664d5afc4
Macarena Ferrero <macarenaferrero@hotmail.com>
2021-04-16 15:31:59 -0300

    Ej. 38

---
+++ Ejercicio_38/Ejercicio_38/Sobreescrito.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_38
+{
+    class Sobreescrito
+    {
+        public override string ToString()
+        {
+            return "Este es mi metodo sobreescrito!";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+    }
+}
