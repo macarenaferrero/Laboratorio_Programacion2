bcf70c4ea5920be9611c30f6b9994d14d8e79bb6
Macarena Ferrero <macarenaferrero@hotmail.com>
2021-05-23 18:47:57 -0300

    Ej 44 OK

---
+++ Ejercicio_44/TestUnitario/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitario")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnitario")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3902975f-7264-4fb0-9f71-530d86a7b47b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Ejercicio_44/TestUnitario/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CentralitaHerencia;
+
+namespace TestUnitario
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test01_Validar_Lista_Instanciada()
+        {
+            //Arrange (Para instanciar lo que hay que probar)
+            Centralita auxTest = new Centralita("FacuPower");
+
+            //Act (Llamar metodo a probar y que arroje valor)
+
+            //Assert (Validacion)
+            Assert.IsNotNull(auxTest.Llamadas);
+            //Assert.AreNotEqual(null, auxTest.Llamadas);
+            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CentralitaException))]
+        public void Test02_Lanzar_Exception_Al_Realizar_Llamada_Existente_Local()
+        {
+            //Arrange
+            Centralita auxTest = new Centralita("FacuPower");
+            Local llamada1 = new Local(0.5f, "123", "456", 3);
+            Local llamada2 = new Local(0.5f, "123", "456", 3);
+
+            //Act
+            auxTest += llamada1;
+            auxTest += llamada2;
+
+            //Assert
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(CentralitaException))]
+        public void Test03_Lanzar_Exception_Al_Realizar_Llamada_Existente_Provincial()
+        {
+            //Arrange
+            Centralita auxTest = new Centralita("FacuPower");
+            Provincial llamada1 = new Provincial ("123", Provincial.Franja.Franja_1 ,0.5f, "456");
+            Provincial llamada2 = new Provincial("123", Provincial.Franja.Franja_2, 0.6f, "456");
+
+            //Act
+            auxTest += llamada1;
+            auxTest += llamada2;
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void Test04_Validar_Igual_Solo_Entre_Llamadas_Del_Mismo_Tipo()
+        {
+            //Arrange
+            Provincial llamada1 = new Provincial("123", Provincial.Franja.Franja_1, 0.5f, "456");
+            Provincial llamada2 = new Provincial("123", Provincial.Franja.Franja_2, 0.6f, "456");
+            Local llamada3 = new Local(0.5f, "456", "123", 3);
+            Local llamada4 = new Local(0.5f, "456", "123", 3);
+            //Act
+
+            //Asert
+            Assert.AreEqual(llamada1, llamada2);
+            Assert.AreEqual(llamada3, llamada4);
+            Assert.AreNotEqual(llamada1, llamada4);
+            Assert.AreNotEqual(llamada2, llamada3);
+            Assert.AreNotEqual(llamada2, llamada4);
+            Assert.AreNotEqual(llamada2, llamada3);
+        }
+
+    }
+}
